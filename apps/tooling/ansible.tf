81523b0d105fbeeb5aa702f8464cbdbfbbedd258
James Nesbitt <jnesbitt@mirantis.com>
2023-12-13 11:40:43 +0200

    cleanup of k0sctl resource
    Signed-off-by: James Nesbitt <jnesbitt@mirantis.com>

--- apps/tooling/ansible.tf
+++ apps/tooling/ansible.tf
@@ -1,13 +1,29 @@
 locals {
+  role_nodegroups = { for r in distinct([for ng in local.nodegroups : ng.role]) : r => [
+    for l, ng in local.nodegroups : l if ng.role == r
+  ] }
+
   ansible_inventory = <<-EOT
-  %{~for ng in local.nodegroups}
-  [${ng.name}]
-  %{~for n in ng.nodes~}
-  %{~if ng.connection == "ssh"~}
-  ${n.label} ansible_connection=ssh ansible_ssh_private_key_file=${n.key_path_abs} ansible_user=${ng.ssh_user} ansible_host=${n.public_address}
-  %{~endif~}
-  %{~endfor}
-  %{~endfor~}
+%{for ng in local.nodegroups~}
+${ng.name}:
+  hosts:
+%{for n in ng.nodes~}
+%{if ng.connection == "ssh"~}
+    ${n.label}:
+      ansible_connection: ssh
+      ansible_ssh_private_key_file: ${n.key_path_abs}
+      ansible_user: ${ng.ssh_user}
+      ansible_host: ${n.public_address}
+%{endif~}
+%{endfor}
+%{endfor}
+%{for r, ngs in local.role_nodegroups~}
+${r}:
+  children:
+%{for ng in ngs~}
+    ${ng}:
+%{endfor}
+%{endfor~}
   EOT
 }
 
@@ -19,23 +35,23 @@ output "ansible_inventory" {
 # Create Ansible inventory file
 resource "local_file" "ansible_inventory" {
   content  = local.ansible_inventory
-  filename = "hosts.ini"
+  filename = "inventory.yml"
 }
 
 resource "local_file" "ansible" {
-  content = <<EOF
+  content         = <<EOF
 SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
-ANSIBLE_CONFIG=$SCRIPT_DIR/ansible.cfg ansible --inventory=$SCRIPT_DIR/hosts.ini $@
+ANSIBLE_CONFIG=$SCRIPT_DIR/ansible.cfg ansible --inventory=$SCRIPT_DIR/inventory.yml $@
 EOF
-  filename = "ansible"
+  filename        = "ansible"
   file_permission = 0777
 }
 
 resource "local_file" "ansible_console" {
-  content = <<EOF
+  content         = <<EOF
 SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
-ANSIBLE_CONFIG=$SCRIPT_DIR/ansible.cfg ansible-console --inventory=$SCRIPT_DIR/hosts.ini $@
+ANSIBLE_CONFIG=$SCRIPT_DIR/ansible.cfg ansible-console --inventory=$SCRIPT_DIR/inventory.yml $@
 EOF
-  filename = "ansible-console"
+  filename        = "ansible-console"
   file_permission = 0777
 }
